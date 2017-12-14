c5d183dc09bac7469d204ee7301d4dd84038b98d
paulaperoutka <paulaperoutka@gmail.com>
2017-12-14 17:35:25 -0500

    additional-js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,12 +9,59 @@
 
 window.onload = function () {
 
-	//Choose words to include
-	var wordList = ["piano", "cello", "music", "notes", "song", "score", "tempo", "rhythm", "orchestra", "percussion"];
-	//declaring variables
-	var wins = 0;
-	var remainingGuesses = 8;
-	var guessedLetters = [];
-	var currentWord = wordList[Math.floor(Math.random() * wordList.length)];
-	var letters = Array.from(wordList);
+//Choose words to include in the game
+var wordList = ["piano", "cello", "music", "notes", "song", "score", "tempo", "rhythm", "orchestra", "percussion"];
+//Select a random word for the game from the wordList array
+var gameWord = wordList[Math.floor(Math.random() * wordList.length)];
+//Get array of letters from game word
+var getLetters = Array.from(gameWord);
+console.log(getLetters)
+
+//Set wins value at zero
+var wins = 0;
+//Set total guesses
+var guessesRemaining = 8;
+//Open variable to store letters not in gameword
+var incorrectGuesses = [];
+
+//Show starting values
+//document.getElementById('displayWord').innerHTML = displayWord;
+
+
+document.getElementById('gamesWon').innerHTML = wins; 
+
+document.getElementById('remainingGuesses').innerHTML = guessesRemaining;
+
+document.getElementById('guessedLetters').innerHTML = incorrectGuesses;
+
+
+document.onkeypress = function playGame (event) {
 	
+
+var displayWord = [];
+		for (var i = 0; i < getLetters.length; i++) {
+	 	displayAnswer[i] = "_";
+	 	}
+
+
+div.innerHTML
+
+
+
+
+
+		var playerGuess
+
+
+
+
+	}
+
+//Close windows.onload function
+}
+
+
+
+
+//a keycode=65
+//z keycode=90
