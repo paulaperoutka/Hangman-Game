4b95055f874de6e8a001c1250f52369d99bcdf11
paulaperoutka <paulaperoutka@gmail.com>
2017-12-16 08:29:00 -0500

    with uncoded logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,3 @@
-//Generate a random word
-//var getWord = wordList[Math.floor(Math.random() * (wordList.length))];
-//Split the word into an array of letters for the game
-//var gameWord = Array.from(getWord);
-
-//var guessedLetters = [];
-//var remainingGuesses = [];
-//var wins = [];
-
-//
-
 //generate a word at random and store it in a variable
 //# display the length of the word to the user
 //# correct_guesses is less than the length of the word
@@ -20,7 +9,11 @@
 //# they lost and exit the program
 //# if correct_guesses is equal to the length of the word, tell the user they won
 
+
+
 window.onload = function () { }
+
+//Set game values and variables
 
 //Choose words to include in the game
 var wordList = ["piano", "cello", "music", "notes", "song", "score", "tempo", "rhythm", "orchestra", "percussion"];
@@ -28,12 +21,22 @@ var wordList = ["piano", "cello", "music", "notes", "song", "score", "tempo", "r
 var wins = 0;
 //Set total guesses
 var guessesRemaining = 8;
-//Open variable to store letters not in gameword
-var incorrectGuesses = [];
-//Open array for gameWord letters
+
 var lettersArray = [];
 
-var correctGuess = [];
+var incorrectGuesses = [];
+
+
+	//Show starting values
+	document.getElementById('startGame').innerHTML = ("Press any letter to begin!");
+
+	document.getElementById('lettersArray').innerHTML = lettersArray;
+
+	document.getElementById('gamesWon').innerHTML = wins; 
+
+	document.getElementById('remainingGuesses').innerHTML = guessesRemaining;
+
+	document.getElementById('guessedLetters').innerHTML = incorrectGuesses;
 
 
 
@@ -46,52 +49,63 @@ var lettersArray = Array.from(gameWord);
 
 	for (var i = 0; i < gameWord.length; i++) {
     lettersArray[i] = "_ ";
+    var hideComma = lettersArray.join("");
+			document.getElementById("lettersArray").innerHTML = hideComma;
     console.log(lettersArray[i]);
 	}
 
 
-//Show starting values
-document.getElementById('start-game').innerHTML = ("Press any letter to begin!");
-
-document.getElementById('lettersArray').innerHTML = lettersArray;
-
-	var hideComma = lettersArray.join("");
-		document.getElementById("lettersArray").innerHTML = hideComma;
-
-document.getElementById('gamesWon').innerHTML = wins; 
-
-document.getElementById('remainingGuesses').innerHTML = guessesRemaining;
-
-document.getElementById('guessedLetters').innerHTML = incorrectGuesses;
+var remainingLetters = gameWord.length;
 
 
-function refreshPage {
-	
-};
-
-
+//Cannot get actual game to run :()
 document.onkeyup = function () {
 
 	var playerGuess = String.fromCharCode(event.keyCode).toUpperCase();
-	var correctGuess = false;
+	var correctGuess = [];
 
 	if (event.keyCode > 64 && event.keyCode < 91) {
-    	document.getElementById('start-game').innerHTML = ("");
-    	console.log(event.key);
 
-    	for (var j=0; j < gameWord.length; j++) {
-    		if (gameWord[j] === playerGuess) {
-    			lettersArray[j] = playerGuess;
-    			guessesRemaining --;
+    	document.getElementById('startGame').innerHTML = ("");
+    	document.getElementById('notLetterMessage').innerHTML = ("");
+    	console.log(event.key);
+    }
+
+    else {
+    	document.getElementById('notLetterMessage').innerHTML = ("That's not a letter!");
+    }
+
+//Capture playerGuess letetr and replace underscore with playerGuess if correct guess
+    if (gameWord.includes(playerGuess)) {
+    	for (var j=0; j<gameWord.length; j++) {
+    		if (gameWord[j] === playerguess) {
+    			lettersArray = playerGuess;
+    			remainingLetters--;
     			correctGuess = true;
     		}
     	}
+    }
+//Uncoded: Discount duplicate letters
+ 	//document.getElementById('duplicateMessage').innerHTML = ("You already guessed that letter!")
+ 	//guessesRemaining !-
 
-	}
+//Uncoded: if incorrect guess, push to guessedLetters, guessesRemaining--
+
+//Uncoded: Lost
+ 	//if (incorrectGuesses > 8) {
+        //document.getElementById('loseMessage').innerHTML =("You lose. Guess a new letter start the game over again!");}
+
+//Uncoded: Win
+ 	//if (remainingGuesses === 0) {
+ 		//document.getElementById('winMessage').innerHTML = ("You win! Guess a new letter to start the game over again!")}
+
+ //Uncoded: reset game
+	//putting the reset for set variables and display inside a function gameSet (and then even running gameSet ();) made it not run
+			
 }
 
 
 
-
-//a keycode=65
-//z keycode=90
+//Close window.onload function here or above? Doesn't work when closed here!
+ 
+ 
